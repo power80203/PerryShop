01ae3dec1723b89ce0b39e51630d47a92e744e1f
perrywu <power80203@gmail.com>
2022-06-12 16:37:49 +0800

    1.post 會失敗 2. CSS會跑版 3. spring 不能夠自己 reload

--- ShopWebParent/ShopBackend/src/main/java/com/perry/admin/user/UserController.java
+++ ShopWebParent/ShopBackend/src/main/java/com/perry/admin/user/UserController.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import com.perry.common.entity.Role;
 import com.perry.common.entity.User;
 
 @Controller
@@ -24,6 +26,37 @@ public class UserController {
 		model.addAttribute("listUsers",listUsers);
 		
 		return "users";
+	}
+	
+	
+	@GetMapping("/users/new")
+	public String newUser(Model model) {
+		
+		List<Role> listRoles =ur.listRoles();
+		
+		
+		
+		User user =  new User();
+		user.setEnabled(true);
+		model.addAttribute("user",user);
+		model.addAttribute("listRoles",listRoles);
+		
+		return "user_form";
+		
+	}
+	
+	
+	@PostMapping("/users/save")
+	public String saveUser(User user) {
+		
+		
+		System.out.println("##############");
+		System.out.println(user.getEmail());
+		ur.save(user);
+		
+		
+		return "redirect:/users";
+		
 	}
 	
 
--- ShopWebParent/ShopBackend/src/main/java/com/perry/admin/user/UserService.java
+++ ShopWebParent/ShopBackend/src/main/java/com/perry/admin/user/UserService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.perry.common.entity.Role;
 import com.perry.common.entity.User;
 
 @Service
@@ -14,10 +15,27 @@ public class UserService {
 	@Autowired
 	private UserRepository uRepository;
 	
+	@Autowired
+	private RoleRepository roleRepository;
+	
+
 	public List<User> listAll() {
 		
 		return (List<User>) uRepository.findAll();
 		
 	}
+	
+	public List<Role> listRoles() {
+		
+		return (List<Role>) roleRepository.findAll();
+		
+	}
+
+	public void save(User user) {
+		// TODO Auto-generated method stub
+		uRepository.save(user);
+		
+	}
+	
 
 }
